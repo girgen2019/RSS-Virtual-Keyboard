65f43977eb8aa9eccc475d74166bdf1c632fbf74
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2022-05-09 21:59:48 +0300

    fix: change to fix bugs

--- script.js
+++ script.js
@@ -32,7 +32,8 @@ document.onkeypress = function(event){
   document.querySelectorAll('#keyboard .k-key').forEach(function(element){
         element.classList.remove('active');
     });
-    document.querySelector('#keyboard .k-key[data = "'+event.keyCode +'"] ').classList.add('active');
+   let active = document.querySelector('#keyboard .k-key[data = "'+event.keyCode +'"]');
+   active.classList.add('active');
     display.textContent += event.key;
    
 }
