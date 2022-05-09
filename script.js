7d22cc56bbba0dcd927626696081620aecc618d9
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2022-05-09 17:44:56 +0300

    feat: add script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+const keyboard = [113,119,101,114,116,121,117,105,111,112,91,93,97,115,
+    100,102,103,104,106,107,108,59,39,122,120,99,118,98,110,109,44,46,47,32]
+
+
+// document.onkeypress = function(event){
+// console.log(event)
+// keyboard.push(event.charCode)
+// console.log(event.key)
+// }
+
+function init(){
+    let out = '';
+    for(let i = 0; i < keyboard.length; i++){
+        if(i === 12 || i === 23 || i === 33){
+            out += '<div class= "clearfix"></div>'
+        }
+        out += '<div class = "buttons" data ="'+ keyboard[i] +'">'+String.fromCharCode(keyboard[i])+'</div>';
+        
+    } 
+    
+    document.querySelector('#keyboard').innerHTML = out;
+   
+} 
+init()
+
+let display = document.querySelector('.display');
+
+document.onkeypress = function(event){
+    
+  document.querySelectorAll('#keyboard .buttons').forEach(function(element){
+        element.classList.remove('active');
+    })
+    document.querySelector('#keyboard .buttons[data = "'+event.keyCode +'"] ').classList.add('active')
+    display.textContent += event.key
+   
+}
+
+document.querySelectorAll('#keyboard .buttons').forEach(function(element){
+    element.onclick = function(event){
+        document.querySelectorAll('#keyboard .buttons').forEach(function(element){
+        element.classList.remove('active');    
+    })
+let code = this.getAttribute('data') ;
+this.classList.add('active');
+console.log(code)
+
+    }
+})
+
